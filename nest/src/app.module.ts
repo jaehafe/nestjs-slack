8179f62256f4e2f3764ddb28f3284486f14649ed
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-28 00:37:59 +0900

    feat: ConfigModule, controller

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// const getEnv = async () => {
+//   const res = await axios.get('/비밀키요청')
+//   return {
+//     DATABASE_USER: 'root',
+//     DATABASE_PASSWORD: '123456789',
+//   };
+// };
 
 @Module({
-  imports: [],
+  // imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService], // 의존성 주입할 서비스 등록
 })
 export class AppModule {}
--- nest/src/app.service.ts
+++ nest/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  async getHello() {
+    return this.configService.get('DATABASE_USER');
+    // process.env.DATABASE_USER
   }
 }
