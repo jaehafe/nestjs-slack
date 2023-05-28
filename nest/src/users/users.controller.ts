7d76d0d6e06eaee8ad8632e3f83f3b74fc7a0615
Jaeha Lee <wogkdkrm112@gmail.com>
2023-05-28 16:43:45 +0900

    feat: Body, Query, Param
    1. dms controller, service 생성,
    2. Body, Query, Param 사용법

--- nest/src/users/users.controller.ts
+++ nest/src/users/users.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
   @Post()
-  postUsers() {}
+  // express의 body-parser
+  postUsers(@Body() data: JoinRequestDto) {
+    this.usersService.postUsers(data.email, data.nickname, data.password);
+  }
 
   @Post('login')
   logIn() {}
