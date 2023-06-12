917af91e36fef50d3b48212dbea8fb77d0cbd4df
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-13 04:21:36 +0900

    feat: forFeature()메서드 사용 - Users repository 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { ChannelsModule } from './channels/channels.module';
 import { UsersService } from './users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { UsersController } from './users/users.controller';
+import { Users } from './entities/Users';
 
 @Module({
   imports: [
@@ -19,8 +21,9 @@ import { typeORMConfig } from './configs/typeorm.config';
     WorkspacesModule,
     DmsModule,
     ChannelsModule,
+    TypeOrmModule.forFeature([Users]),
   ],
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService, UsersService], // 의존성 주입할 서비스 등록
   exports: [],
 })
