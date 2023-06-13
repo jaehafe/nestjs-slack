b78aa29de589481d0173703668957c1bea598a06
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-13 19:57:33 +0900

    feat: response에러 핸들링 - HttpExceptionFilter 사용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
@@ -16,18 +16,18 @@ export class UsersService {
   async join(email: string, nickname: string, password: string) {
     if (!email) {
       // 이메일 없다면 에러
-      throw new Error('이메일이 없습니다.');
+      throw new HttpException('이메일이 없습니다.', 400);
     }
     if (!nickname) {
-      throw new Error('닉네임이 없습니다.');
+      throw new HttpException('닉네임이 없습니다.', 400);
     }
     if (!password) {
-      throw new Error('잘못된 비밀번호입니다.');
+      throw new HttpException('잘못된 비밀번호입니다.', 400);
     }
     const user = await this.usersRepository.findOne({ where: { email } });
     if (user) {
       // 이미 존재하는 유저 에러
-      throw new Error('이미 존재하는 사용자입니다.');
+      throw new HttpException('이미 존재하는 사용자입니다.', 400);
     }
 
     const hashPassword = await bcrypt.hash(password, 12);
