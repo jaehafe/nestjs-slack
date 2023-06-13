509e77fcd4d2db056e6222fcbbc460c932de1077
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-14 06:56:59 +0900

    feat: channels service - getWorkspaceChannelMembers, ceateWorkspaceChannelMembers 추가

--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -70,4 +70,52 @@ export class ChannelsService {
     channelMember.ChannelId = channelReturned.id;
     await this.channelMembersRepository.save(channelMember);
   }
+
+  async getWorkspaceChannelMembers(url: string, name: string) {
+    // name 을 가진 channel들의 속해있는 멤버들
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .innerJoin('user.Channels', 'channels', 'channels.name = :name', {
+        name,
+      })
+      .innerJoin('channels.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getMany();
+  }
+
+  async createWorkspaceChannelMembers(
+    url: string,
+    name: string,
+    email: string,
+  ) {
+    const channel = await this.channelsRepository
+      .createQueryBuilder('channel')
+      .innerJoin('channel.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .where('channel.name = :name', { name })
+      .getOne();
+
+    if (!channel) {
+      throw new NotFoundException('채널이 존재하지 않습니다.');
+    }
+
+    const user = await this.usersRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .innerJoin('user.Workspaces', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException('사용자가 존재하지 않습니다.');
+    }
+
+    const channelMember = new ChannelMembers();
+    channelMember.ChannelId = channel.id;
+    channelMember.UserId = user.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
 }
